3390e622a5b3aa552befa023b8a4d9512fcb1087
ForGetASD <vitayz60@gmail.com>
2022-05-19 14:47:30 +0200

    Hajo osztály feltöltése

--- src/torpedoegyszem/Hajo.java
+++ src/torpedoegyszem/Hajo.java
@@ -1,14 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package torpedoegyszem;
 
-/**
- *
- * @author vitay
- */
 public class Hajo {
+    private int[] pozicio = new int[3];
     
+    public static String talalat(int poz){
+        return "";
+    }
 }
