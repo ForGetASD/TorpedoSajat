3ec7aac0600c794b784001cb5e62cf95ebb4603a
ForGetASD <vitayz60@gmail.com>
2022-05-19 14:39:19 +0200

    1. commit

---
+++ src/torpedoegyszem/TorpedoTeszt.java
@@ -0,0 +1,11 @@
+package torpedoegyszem;
+
+public class TorpedoTeszt {
+
+    public static void main(String[] args) {
+
+    }
+    public static String tesztLoves(int poz){
+        return "";
+    }
+}
