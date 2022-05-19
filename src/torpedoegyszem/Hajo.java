c0a17fe79a055b94004310380318a26ed89427b6
ForGetASD <vitayz60@gmail.com>
2022-05-19 15:13:32 +0200

    assert müködés

--- src/torpedoegyszem/Hajo.java
+++ src/torpedoegyszem/Hajo.java
@@ -3,8 +3,13 @@ package torpedoegyszem;
 
 public class Hajo {
     private int[] pozicio = new int[3];
+
+    public Hajo(int[] pozicio) {
+        this.pozicio = pozicio;
+    }
     
-    public static String talalat(int poz){
-        return "találat";
+    public String talalat(int poz){
+        
+        return "talált";
     }
 }
--- src/torpedoegyszem/TorpedoTeszt.java
+++ src/torpedoegyszem/TorpedoTeszt.java
@@ -3,7 +3,8 @@ package torpedoegyszem;
 public class TorpedoTeszt {
 
     public static void main(String[] args) {
-        Hajo hajo = new Hajo();
+        int[] tomb = {2,3,4};
+        Hajo hajo = new Hajo(tomb);
         String t = hajo.talalat(4);
         assert t.equals("talált") : "nem jó a találat ellenőrzése";
     }
