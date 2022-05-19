999b3d1d3627041e505b37e1a3fe81b166f69f2a
ForGetASD <vitayz60@gmail.com>
2022-05-19 14:52:18 +0200

    assert teszt

--- src/torpedoegyszem/Hajo.java
+++ src/torpedoegyszem/Hajo.java
@@ -5,6 +5,6 @@ public class Hajo {
     private int[] pozicio = new int[3];
     
     public static String talalat(int poz){
-        return "";
+        return "találat";
     }
 }
--- src/torpedoegyszem/TorpedoTeszt.java
+++ src/torpedoegyszem/TorpedoTeszt.java
@@ -3,7 +3,9 @@ package torpedoegyszem;
 public class TorpedoTeszt {
 
     public static void main(String[] args) {
-
+        Hajo hajo = new Hajo();
+        String t = hajo.talalat(4);
+        assert t.equals("talált") : "nem jó a találat ellenőrzése";
     }
     public static String tesztLoves(int poz){
         return "";
